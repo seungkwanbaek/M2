5b076e68ad4b326f4049e71591210992db9227ac
Bryan Baek <seungkwan.baek@gmail.com>
2019-01-15 17:44:48 -0500

    Bryan did person1

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Bryan
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,8 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  if (input == null || input.length() < 3) return input;
+	  return input.substring(2) + input.substring(0,2);
 	}
 	
 	/**

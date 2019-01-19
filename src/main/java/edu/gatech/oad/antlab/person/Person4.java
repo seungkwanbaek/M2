c66d5fa2c17d40c61d6e98451d7b5d4b67b6b85e
jysui123 <jysui2014@gmail.com>
2019-01-19 16:26:49 -0500

    implement person4 - Jingyang

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Jingyang
  *  @version 1.1
  */
 public class Person4 {
@@ -31,7 +31,11 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      StringBuilder sb = new StringBuilder();
+      for (char c : input.toCharArray()) {
+        sb.append(++c);
+      }
+      return sb.toString();
     }
     
     /**

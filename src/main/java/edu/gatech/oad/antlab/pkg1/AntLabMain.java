20d6f1b73dd3ba44d44d6bd18e013ea65850e89b
YANG HU <whoyoung@gatech.edu>
2019-01-19 17:47:21 -0500

    filling out names and GtID

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,20 +61,20 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("sbaek47");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
+		  Person2 p2 = new Person2("Young");
+		  toPrint += p2.toString("yhu388");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Yuqi Cao");
+		  toPrint += p3.toString("ycao344");
           //Person4 replace P4 with your name
           //and gburdell4 with your gt id
-          Person4 p4 = new Person4("P4");
-          toPrint += p4.toString("Jingyang Sui");
+          Person4 p4 = new Person4("Jingyang Sui");
+          toPrint += p4.toString("jsui7");
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("gburdell5");
+          Person5 p5 = new Person5("Xibei Zhang");
+          toPrint += p5.toString("xzhang774");
 		  
         System.out.println(toPrint);
         
